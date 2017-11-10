1d1a249d05b649d45d4146ddbb09f5747f227b27
tch180 <thopstadredner@gmail.com>
2017-11-10 15:06:53 -0500

    added NavBar component

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                Hello from the navbar 
+            </div>
+        );
+    }
+}
+
+export default Navbar;
---
+++ db/migrate/20171110193630_create_chores.rb
@@ -0,0 +1,10 @@
+class CreateChores < ActiveRecord::Migration[5.1]
+  def change
+    create_table :chores do |t|
+      t.integer :points
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
