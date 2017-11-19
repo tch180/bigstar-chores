93fe15949fcd1d2f7c9b48e89fa08103162910a2
tch180 <thopstadredner@gmail.com>
2017-11-19 13:53:00 -0500

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Chore.create({
+    points: 50, name: "pull weeds"})
+
+
+    
